34aea4fc56183d3f715aef0c85f4244a023b3c07
dalo2012 <daviddalo2012@gmail.com>
2017-10-01 19:28:17 -0600

    Initial commit

---
+++ app/src/main/java/com/example/davidreetz/firststronghold2016scoutingapp/masonsproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.davidreetz.firststronghold2016scoutingapp.masonsproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
